881dbadb03286a80bb5a175ae98d2088ec2eb029
adminyh <1658431245@qq.com>
2019-07-07 15:49:08 +0800

    regularization logisitic regression

--- homework/WEEK3/machine-learning-ex2/ex2/costFunctionReg.m
+++ homework/WEEK3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -16,10 +16,10 @@ grad = zeros(size(theta));
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
-
-
-
-
+h = sigmoid(X*theta);
+J = lambda/(2*m) * sum(theta(2:length(theta)).^2) -sum(y.*log(h)+(1-y).*log(1-h))/m;
+grad(1) = (X(:,1)'*(h-y))./m;
+grad(2:length(theta)) = (X(:,2:length(theta))'*(h-y))./m + lambda/m*theta(2:length(theta));
 
 
 % =============================================================
