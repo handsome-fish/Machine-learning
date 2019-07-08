eecbb7dca75920dc98f9c475edb587193b07a532
adminyh <1658431245@qq.com>
2019-07-08 17:00:36 +0800

    implement lrCostFunction in WEEK4

--- homework/WEEK4/machine-learning-ex3/ex3/lrCostFunction.m
+++ homework/WEEK4/machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,14 +35,10 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
-
-
-
-
-
-
-
-
+h = sigmoid(X*theta);
+J = sum(-y.*log(h)-(1-y).*log(1-h)) / m + lambda/(2*m) * sum(theta(2:length(theta)).^2);
+grad(1) = (X(:,1)'*(h-y)) ./ m;
+grad(2:length(theta)) = (X(:,2:length(theta))'*(h-y)) ./ m + lambda/m * sum(theta(2:length(theta)));
 
 
 % =============================================================
