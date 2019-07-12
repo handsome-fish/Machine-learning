515dae9b772eb4ee77896100314aebf3c93a47aa
adminyh <1658431245@qq.com>
2019-07-12 23:59:41 +0800

    implement regularized linear regression cost function

--- homework/WEEK6/machine-learning-ex5/ex5/linearRegCostFunction.m
+++ homework/WEEK6/machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,16 +18,8 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-
-
-
-
-
-
-
-
-
-
+%计算cost,正则化除去θ0
+J = 1/(2*m) * sum((X*theta - y).^2) + lambda/(2*m) * sum(theta(2:end).^2);
 
 
 % =========================================================================
