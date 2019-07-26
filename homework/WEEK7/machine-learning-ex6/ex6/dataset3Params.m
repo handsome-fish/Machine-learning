5bb50b5cca8a631ad56a16185a48c44aefbec170
adminyh <1658431245@qq.com>
2019-07-26 20:05:37 +0800

    implement data3Params.m

--- homework/WEEK7/machine-learning-ex6/ex6/dataset3Params.m
+++ homework/WEEK7/machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,30 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+steps = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+cost = 1000;
+op_C = 0.01;
+op_sigma = 0.01;
 
+for i = 1:8,
+  for j = 1:8,
 
-
-
-
+    model= svmTrain(X, y, steps(i), @(x1, x2) gaussianKernel(x1, x2, steps(j)));
+    predictions = svmPredict(model, Xval);
+    temp_cost = mean(double(predictions ~= yval));
+    if cost > temp_cost,
+      op_C = steps(i);
+      op_sigma = steps(j);
+      cost = temp_cost;
+    endif
+    fprintf('Step: %d\tC: %f\tsigma: %f\ttemp_cost: %f\tnew_cost: %f\n',
+      (i-1)*8+j, steps(i), steps(j), temp_cost, cost);    
+    j += 1;
+  endfor
+  i += 1;
+endfor
+C = op_C;
+sigma = op_sigma;
 
 % =========================================================================
 
--- homework/WEEK7/machine-learning-ex6/ex6/ex6.m
+++ homework/WEEK7/machine-learning-ex6/ex6/ex6.m
@@ -107,8 +107,8 @@ C = 1; sigma = 0.1;
 % We set the tolerance and max_passes lower here so that the code will run
 % faster. However, in practice, you will want to run the training to
 % convergence.
-model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
-visualizeBoundary(X, y, model);
+% model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+% visualizeBoundary(X, y, model);
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
@@ -143,6 +143,7 @@ load('ex6data3.mat');
 
 % Try different SVM Parameters here
 [C, sigma] = dataset3Params(X, y, Xval, yval);
+fprintf('optimal C = %f and sigma = %f\n', C, sigma);
 
 % Train the SVM
 model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
